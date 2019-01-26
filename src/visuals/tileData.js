db28ecb6142a294d11f8a381e84d769519d767a0
lkire <alkire@gmail.com>
2019-01-26 08:00:37 +0000

    basis for visualization cards --remove grid

---
+++ src/visuals/tileData.js
@@ -0,0 +1,24 @@
+import image from './profile_smaller.jpg';
+
+const tileData = [
+  	{
+    	img: image,
+ 		title: 'Feature',
+		author: 'author',
+		featured: true,
+	},
+	{
+    	img: image,
+ 		title: 'Something',
+		author: 'author',
+		featured: false,
+	},
+	{
+    	img: image,
+ 		title: 'Else',
+		author: 'author',
+		featured: false,
+	},
+];
+
+export default tileData;
