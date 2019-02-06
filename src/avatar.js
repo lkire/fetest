c5157b0e663784d23fde4269482b934e9bb60343
lkire <alkire@gmail.com>
2019-02-06 23:38:12 +0000

    cleaned imports

--- src/avatar.js
+++ src/avatar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
-import Grid from '@material-ui/core/Grid';
 import prof from './profile_smaller.jpg';
 
 const styles = {
