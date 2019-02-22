27a85df43d5845224dc5744d606d16ab56d160fd
lkire <alkire@gmail.com>
2019-02-22 18:04:50 +0000

    initial models stubs

---
+++ src/models/cardData.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+import leopard from './leopard.jpg'
+import forest from './forest.jpg'
+import baby from './baby.jpg'
+import dune from './dune.jpg'
+import storm from './storm.jpg'
+
+const cardData = [
+  	{
+    	image: leopard,
+ 		title: 'Convolutional Neural Networks',
+ 		txtfile: 'default.md',
+	},
+	{
+    	image: forest,
+ 		title: 'Random Forests',
+ 		txtfile: 'default.md',
+	},
+	{
+    	image: baby,
+ 		title: 'Generative Adversarial Networks',
+ 		txtfile: 'default.md',
+	},
+	{
+    	image: storm,
+ 		title: 'Asymptotic Likelihood Theory',
+ 		txtfile: 'default.md',
+	},
+	{
+    	image: dune,
+ 		title: 'Time Series Wavelet Analysis',
+ 		txtfile: 'default.md',
+	},
+
+
+];
+
+export default cardData;
