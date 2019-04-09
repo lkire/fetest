ee6ff8cc751bedf8a70cd50840ff7da5d24185e4
lkire <alkire@gmail.com>
2019-04-09 23:25:16 +0000

    writeup

---
+++ src/visuals/txtcvxad.js
@@ -0,0 +1,9 @@
+export default  String.raw`
+## Custom code renderer
+This example shows how you can make react-markdown syntax-highlight code blocks.
+By default, react-markdown will simply wrap code blocks in <pre><code>.
+
+$$
+\int^{y} 
+$$
+`;
