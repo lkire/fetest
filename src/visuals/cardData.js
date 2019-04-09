56d7b238e7a9077aecf7fc3b7ae15bc310600f00
lkire <alkire@gmail.com>
2019-04-09 22:58:35 +0000

    click through optimization added

--- src/visuals/cardData.js
+++ src/visuals/cardData.js
@@ -3,8 +3,46 @@ import Typography from '@material-ui/core/Typography';
 
 const cardData = [
   	{
+    	source: 'http://lkire.com:5006/cvxad',
+ 		title: 'Click-through Ad. Optimization',
+ 		text: () => (
+ 		  <div>
+		    <Typography paragraph>Method:</Typography>
+	        <Typography paragraph>
+	          Leave this on for a few minutes and crash your browser and my server!
+	        </Typography>
+          </div>
+ 		),
+	},
+    
+	{
     	source: 'http://demo.ryptco.com:5006/testapp',
- 		title: 'Memory Destroyer',
+ 		title: 'Weather',
+ 		text: () => (
+ 		  <div>
+		    <Typography paragraph>Method:</Typography>
+	        <Typography paragraph>
+	          Leave this on for a few minutes and crash your browser and my server!
+	        </Typography>
+          </div>
+ 		),
+	},
+
+        {
+    	source: 'http://demo.ryptco.com:5006/testapp',
+ 		title: 'Imaging',
+ 		text: () => (
+ 		  <div>
+		    <Typography paragraph>Method:</Typography>
+	        <Typography paragraph>
+	          Leave this on for a few minutes and crash your browser and my server!
+	        </Typography>
+          </div>
+ 		),
+	},
+	{
+    	source: 'http://demo.ryptco.com:5006/testapp',
+ 		title: 'Hypothesis Testing',
  		text: () => (
  		  <div>
 		    <Typography paragraph>Method:</Typography>
