cab099bd2ea469c9dae599dec5c256e118f9d73f
lkire <alkire@gmail.com>
2019-04-10 23:59:32 +0000

    convex writeup

--- src/advertising/txtcvxad.js
+++ src/advertising/txtcvxad.js
@@ -1,9 +1,26 @@
 export default  String.raw`
-## Custom code renderer
-This example shows how you can make react-markdown syntax-highlight code blocks.
-By default, react-markdown will simply wrap code blocks in <pre><code>.
+## Cost-per-click Model
 
-$$
-\int^{y} 
-$$
+Cost-per-click advertising pays the *publisher* (a website) when an ad is clicked.
+In the model simulated above, there are independent contracts for each advertisement
+that specify the cost $C_i$ paid to the publisher each time an advertisement $i$ is clicked by a visitor.
+Each contract also comes with a specification of the minimum number of ads $m_i$ to show
+each day and the maximum available daily budget $B_i$. Together these variables
+are the three columns of sliders above.
+
+Given this type of contract it is natural for the publisher to ask the question: 
+> **'How can we maximize our ad revenue?'** 
+
+The answer depends not only on the constraints specified but also on the 
+expected chance $p_t$ of a customer clicking an advertisement and the expected
+number of hourly visitors $v_t$. Each are parameterized by hour $t$ and in this
+example are fabricated--refer to the tabs above to see the data.
+
+This is clearly a simplified problem, not taking into account relevant aspects
+like customer profiling and uncertainties on predictions.
+
+## Convex Optimization
+As described, maximizing ad revenue is a convex optimization problem that can 
+be solved *efficiently*. 
+
 `;
